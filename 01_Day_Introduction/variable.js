dd3efb8e52924aaf58451e41e0ccdaeeb7a1763b
narayan2111 <narayandwivedi076gmail.com>
2023-08-09 16:22:35 +0530

    Day 1 Completed

--- 01_Day_Introduction/variable.js
+++ 01_Day_Introduction/variable.js
@@ -1,17 +1,8 @@
-// Declaring different variables of different data types
-let firstName = 'Asabeneh' // first name of a person
-let lastName = 'Yetayeh' // last name of a person
-let country = 'Finland' // country
-let city = 'Helsinki' // capital city
-let age = 100 // age in years
-let isMarried = true
+//Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
 
-// Declaring variables with number values
-const gravity = 9.81 // earth gravity  in m/s2
-const boilingPoint = 100 // water boiling point, temperature in oC
-const PI = 3.14 // geometrical constant
+let name = 'Narayan'
+let boolValue = true
+let UNdefined
+let nullValue = null
 
-// Variables can also be declaring in one line separated by comma
-let name = 'Asabeneh', //name of a person
-  job = 'teacher',
-  live = 'Finland'
+console.log(name,boolValue,UNdefined,nullValue)
