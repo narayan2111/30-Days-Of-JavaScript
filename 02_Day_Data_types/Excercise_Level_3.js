f1cbc1d2c151de2f9db7ef0dd2bae34126cfabd5
narayan2111 <narayandwivedi076gmail.com>
2023-08-10 23:56:44 +0530

    Day2

---
+++ 02_Day_Data_types/Excercise_Level_3.js
@@ -0,0 +1,4 @@
+// 1. 'Love is the best thing in this world. Some found their love and some are still looking for their love.' 
+// Count the number of word love in this sentence.
+
+let string = `Love is the best thing in this world. Some found their love and some are still looking for their love.` 
--- 02_Day_Data_types/string_concatenation.js
+++ 02_Day_Data_types/string_concatenation.js
@@ -1,4 +1,4 @@
-// Declaring different variables of different data types
+/*/ Declaring different variables of different data types
 let space = ' '
 let firstName = 'Asabeneh'
 let lastName = 'Yetayeh'
@@ -17,3 +17,31 @@ let personInfoTwo = `I am ${fullName}. I am ${age}. I live in ${country}.` //ES6
 let personInfoThree = `I am ${fullName}. I live in ${city}, ${country}. I am a ${job}. I teach ${language}.`
 console.log(personInfoTwo)
 console.log(personInfoThree)
+
+let a =5
+let b = 6
+console.log(`Sum of ${a} and ${b} is ${a+b}`)
+console.log(`${a} is greater than ${b}: ${a>b}`)
+let js = `javascript is a object oriented programming language`
+console.log(js.length)
+console.log(js[9])
+console.log(js.toUpperCase())
+let LOWERCASE = `HELLO WORLD`
+console.log(LOWERCASE.toLowerCase())
+console.log(js.substr(3,6))
+console.log(js.substring(3,6))
+let string = `   Welcome guys  `
+console.log(string)
+console.log(string.length)
+console.log(string.trim())
+console.log(string.includes('Guys'))
+console.log(string.replace(`Welcome`,`Hii`))
+let NewString = `Lakshmi Narayan Dwivedi`
+let tempString = /Narayan/
+console.log(NewString.match(tempString))
+console.log(string.repeat(4))
+*/
+
+let number = '10'
+let num = parseFloat(number)
+console.log(num)
